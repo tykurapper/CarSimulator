a900e66cc361c25642d10da016f47f67f78cf6fe
tyku <tykurapper@gmail.com>
2018-01-07 12:18:16 +0700

    them diem de demo

--- src/states/SimulatorState.java
+++ src/states/SimulatorState.java
@@ -25,9 +25,12 @@ public class SimulatorState extends State{
 	private Queue<TurningPoint> stack = new LinkedList<TurningPoint>();
 
 	public SimulatorState(){
+//		case 1
 		car = new Car(0, 20);
 		stack.add(new ArcTurningPoint(new Point(180, 100), TurningPoint.DEFAULT_RADIUS, new QuadCurve2D.Float(0, 0, 200, 0, 200, 100), new QuadCurve2D.Float(0, 40, 160, 40, 160, 100)));
 		stack.add(new StraightTurningPoint(new Point(180, 200), TurningPoint.DEFAULT_RADIUS, new Line(200, 100, 200, 200), new Line(160, 100, 160, 200)));
+		stack.add(new ArcTurningPoint(new Point(300, 280), TurningPoint.DEFAULT_RADIUS, new QuadCurve2D.Float(200, 200, 200, 260, 300, 260), new QuadCurve2D.Float(160, 200, 160, 300, 300, 300)));
+//		case 2
 //		stack.add(new StraightTurningPoint(new Point(300, 40), TurningPoint.DEFAULT_RADIUS, new Line(40, 20, 300, 20), new Line(40, 60, 300, 60)));
 //		stack.add(new StraightTurningPoint(new Point(600, 40), TurningPoint.DEFAULT_RADIUS, new Line(300, 20, 600, 20), new Line(300, 60, 600, 60)));
 //		stack.add(new StraightTurningPoint(new Point(600, 400), TurningPoint.DEFAULT_RADIUS, new Line(600, 20, 500, 400), new Line(540, 20, 440, 400)));
