094cccd01d896cc3f3c6bd3842049b0887337b80
tyku <tykurapper@gmail.com>
2018-04-14 23:20:05 +0700

    Giam bot so luat
    Giam luat traffic light tu 80 xuong 38
    Tang kich thuoc canvas len 800x600

--- src/core/Launcher.java
+++ src/core/Launcher.java
@@ -5,7 +5,7 @@ import display.Display;
 public class Launcher {
 	public static void main(String[] args) {
 		
-		Simulator simulator = new Simulator("CarSimulator", 680, 360);
+		Simulator simulator = new Simulator("CarSimulator", 800, 600);
 		simulator.start();
 	}
 }
